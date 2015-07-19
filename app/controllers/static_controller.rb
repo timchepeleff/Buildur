dae909bdac832f1a52b214209c3fdc707f1397bb
Tim Chepeleff <tim.chepeleff@gmail.com>
2015-07-19 18:14:38 -0400

    Update static_controller.rb

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,7 @@
 class StaticController < ApplicationController
   def index
-      unless current_user.profile_edited?
-        redirect_to edit_user_path(current_user)
-      end
+    unless current_user.profile_edited?
+      redirect_to edit_user_path(current_user)
+    end
   end
 end
