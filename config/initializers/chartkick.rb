ea4755b1520425f77ca8d29545e714b6473a4bde
Tim Chepeleff <tim.chepeleff@gmail.com>
2015-07-19 17:26:21 -0400

    hound issues #1

--- config/initializers/chartkick.rb
+++ config/initializers/chartkick.rb
@@ -1,184 +1,186 @@
 Chartkick.options = { library:
     { legend:
         { enabled: true },
-        colors: ["#A9467A","#CF6456","#ECF8D7","#A5D7B2","#D58CB2", "#FFB2A8", "#82C594", "#D2EF9D", "#0088CC"],
+        colors: ["#A9467A","#CF6456","#ECF8D7","#A5D7B2",
+                 "#D58CB2", "#FFB2A8", "#82C594", "#D2EF9D",
+                 "#0088CC"],
        chart: {
-      backgroundColor: "transparent",
+            backgroundColor: "transparent",
       style: {
-         fontFamily: "'Unica One', sans-serif"
+         fontFamily: "Unica One, sans-serif"
       },
-      plotBorderColor: '#606063'
+      plotBorderColor: "#606063"
    },
    title: {
       style: {
-         color: '#0088CC',
-         textTransform: 'uppercase',
-         fontSize: '20px'
+         color: "#0088CC",
+         textTransform: "uppercase",
+         fontSize: "20px"
       }
    },
    subtitle: {
       style: {
-         color: '#0088CC',
-         textTransform: 'uppercase'
+               color: "#0088CC",
+               textTransform: "uppercase"
       }
    },
  xAxis: {
-      gridLineColor: '#707073',
+      gridLineColor: "#707073",
       labels: {
          style: {
-            color: '#E0E0E3'
+            color: "#E0E0E3"
          }
       },
-      lineColor: '#707073',
-      minorGridLineColor: '#505053',
-      tickColor: '#707073',
+      lineColor: "#707073",
+      minorGridLineColor: "#505053",
+      tickColor: "#707073",
       title: {
          style: {
-            color: '#A0A0A3'
+            color: "#A0A0A3"
 
          }
       }
    },
    yAxis: {
-      gridLineColor: '#707073',
+      gridLineColor: "#707073",
       labels: {
          style: {
-            color: '#E0E0E3'
+            color: "#E0E0E3"
          }
       },
-      lineColor: '#707073',
-      minorGridLineColor: '#505053',
-      tickColor: '#707073',
+      lineColor: "#707073",
+      minorGridLineColor: "#505053",
+      tickColor: "#707073",
       tickWidth: 1,
       title: {
          style: {
-            color: '#A0A0A3'
+            color: "#A0A0A3"
          }
       }
    },
    tooltip: {
-      backgroundColor: 'rgba(0, 0, 0, 0.85)',
+      backgroundColor: "rgba(0, 0, 0, 0.85)",
       style: {
-         color: '#F0F0F0'
+         color: "#F0F0F0"
       }
    },
    plotOptions: {
       series: {
          dataLabels: {
-            color: '#000'
+            color: "#000"
          },
          marker: {
-            lineColor: '#333'
+            lineColor: "#333"
          }
       },
       boxplot: {
-         fillColor: '#505053'
+         fillColor: "#505053"
       },
       candlestick: {
-         lineColor: 'white'
+         lineColor: "white"
       },
       errorbar: {
-         color: 'white'
+         color: "white"
       }
    },
    legend: {
       itemStyle: {
-         color: '#E0E0E3'
+         color: "#E0E0E3"
       },
       itemHoverStyle: {
-         color: '#FFF'
+         color: "#FFF"
       },
       itemHiddenStyle: {
-         color: '#606063'
+         color: "#606063"
       }
    },
    credits: {
       style: {
-         color: '#666'
+         color: "#666"
       }
    },
    labels: {
       style: {
-         color: '#0088CC'
+         color: "#0088CC"
       }
    },
 
    drilldown: {
       activeAxisLabelStyle: {
-         color: '#F0F0F3'
+         color: "#F0F0F3"
       },
       activeDataLabelStyle: {
-         color: '#F0F0F3'
+         color: "#F0F0F3"
       }
    },
 
    navigation: {
       buttonOptions: {
-         symbolStroke: '#DDDDDD',
+         symbolStroke: "#DDDDDD",
          theme: {
-            fill: '#505053'
+            fill: "#505053"
          }
       }
    },
    rangeSelector: {
       buttonTheme: {
-         fill: '#505053',
-         stroke: '#000000',
+         fill: "#505053",
+         stroke: "#000000",
          style: {
-            color: '#CCC'
+            color: "#CCC"
          },
          states: {
             hover: {
-               fill: '#707073',
-               stroke: '#000000',
+               fill: "#707073",
+               stroke: "#000000",
                style: {
-                  color: 'white'
+                  color: "white"
                }
             },
             select: {
-               fill: '#000003',
-               stroke: '#000000',
+               fill: "#000003",
+               stroke: "#000000",
                style: {
-                  color: 'white'
+                  color: "white"
                }
             }
          }
       },
-      inputBoxBorderColor: '#505053',
+      inputBoxBorderColor: "#505053",
       inputStyle: {
-         backgroundColor: '#333',
-         color: 'silver'
+         backgroundColor: "#333",
+         color: "silver"
       },
       labelStyle: {
-         color: '#D8D8D8'
+         color: "#D8D8D8"
       }
    },
 
    navigator: {
       handles: {
-         backgroundColor: '#666',
-         borderColor: '#AAA'
+         backgroundColor: "#666",
+         borderColor: "#AAA"
       },
-      outlineColor: '#CCC',
-      maskFill: 'rgba(255,255,255,0.1)',
+      outlineColor: "#CCC",
+      maskFill: "rgba(255,255,255,0.1)",
       series: {
-         color: '#7798BF',
-         lineColor: '#A6C7ED'
+         color: "#7798BF",
+         lineColor: "#A6C7ED"
       },
       xAxis: {
-         gridLineColor: '#505053'
+         gridLineColor: "#505053"
       }
    },
 
    scrollbar: {
-      barBackgroundColor: '#808083',
-      barBorderColor: '#808083',
-      buttonArrowColor: '#CCC',
-      buttonBackgroundColor: '#606063',
-      buttonBorderColor: '#606063',
-      rifleColor: '#FFF',
-      trackBackgroundColor: '#404043',
-      trackBorderColor: '#404043'
+      barBackgroundColor: "#808083",
+      barBorderColor: "#808083",
+      buttonArrowColor: "#CCC",
+      buttonBackgroundColor: "#606063",
+      buttonBorderColor: "#606063",
+      rifleColor: "#FFF",
+      trackBackgroundColor: "#404043",
+      trackBorderColor: "#404043"
    },
 
 }
