c4a93eb25bffec97b0ba8287646ec5b609b6ac35
Tim Chepeleff <tim.chepeleff@gmail.com>
2015-07-20 17:18:23 -0400

    hound issues

--- db/migrate/20150720031745_create_user_skills.rb
+++ db/migrate/20150720031745_create_user_skills.rb
@@ -1,8 +1,8 @@
 class CreateUserSkills < ActiveRecord::Migration
   def change
     create_table :user_skills do |t|
-      t.integer :user_id, null:false
-      t.integer :skill_id, null:false
+      t.integer :user_id, null: false
+      t.integer :skill_id, null: false
 
       t.timestamps null: false
     end
--- db/migrate/20150720183738_create_user_preferences.rb
+++ db/migrate/20150720183738_create_user_preferences.rb
@@ -1,8 +1,8 @@
 class CreateUserPreferences < ActiveRecord::Migration
   def change
     create_table :user_preferences do |t|
-      t.integer :user_id, null:false
-      t.integer :preference_id, null:false
+      t.integer :user_id, null: false
+      t.integer :preference_id, null: false
 
       t.timestamps null: false
     end
